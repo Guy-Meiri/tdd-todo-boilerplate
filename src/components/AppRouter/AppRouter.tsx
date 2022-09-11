35d91029aa0a4ef8382ce975aa002b3305e30a70
Guy Meiri <guymeiri@microsoft.com>
2022-09-11 23:52:10 +0300

    passed new todo route test

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -5,24 +5,35 @@ import { About } from '../About/About';
 import { Home } from './Routes/Home';
 import { NoMatch } from './Routes/NoMatch';
 import s from './AppRouter.module.css';
+import Todo from '../Todo/Todo';
 
 export const LocationDisplay = () => {
   const location = useLocation();
 
-  return <div data-hook='location-display'>{location.pathname}</div>;
+  return <div data-hook="location-display">{location.pathname}</div>;
 };
 
 export const AppRouter = () => (
-  <div className={s.navRoutes} data-hook='app-router'>
-    <Link className={s.link} data-hook='home-link' to='/'>Home</Link>
-    <Link className={s.link} data-hook='about-link' to='/about'>About</Link>
-    <Link className={s.link} data-hook='counter-link' to='/counter'>Counter</Link>
+  <div className={s.navRoutes} data-hook="app-router">
+    <Link className={s.link} data-hook="home-link" to="/">
+      Home
+    </Link>
+    <Link className={s.link} data-hook="about-link" to="/about">
+      About
+    </Link>
+    <Link className={s.link} data-hook="counter-link" to="/counter">
+      Counter
+    </Link>
+    <Link className={s.link} data-hook="todo-link" to="/todo">
+      Todo
+    </Link>
 
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/counter' element={<Counter />} />
-      <Route path='*' element={<NoMatch />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/counter" element={<Counter />} />
+      <Route path="/todo" element={<Todo />} />
+      <Route path="*" element={<NoMatch />} />
     </Routes>
 
     <LocationDisplay />
