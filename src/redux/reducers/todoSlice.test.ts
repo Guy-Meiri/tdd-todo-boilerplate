f26695d6720f3314dd0875d1563a579a3dfb5200
Guy Meiri <guymeiri@microsoft.com>
2022-09-12 22:19:40 +0300

    added failing test for deleting a todo item

--- src/redux/reducers/todoSlice.test.ts
+++ src/redux/reducers/todoSlice.test.ts
@@ -1,5 +1,5 @@
 import { createMockStore } from '../store/mockStore';
-import { add, getTodos, TodoItem, todoReducer } from './todoSlice';
+import { add, delete, getTodos, TodoItem, todoReducer } from './todoSlice';
 
 const todoItem1: TodoItem = { id: '1', content: 'todo item 1' };
 describe('todoSlice', () => {
@@ -10,5 +10,14 @@ describe('todoSlice', () => {
     });
     store.dispatch(add(todoItem1));
     expect(getTodos(store.getState().todo)).toEqual([todoItem1]);
+  });
+
+  it('should delete an existing todo item', () => {
+    const { store } = createMockStore({
+      preloadedState: { todo: [todoItem1] },
+      reducer: { todo: todoReducer },
+    });
+    store.dispatch(delete(todoItem1.id));
+    expect(getTodos(store.getState().todo)).toEqual([]);
   });
 });
