f84097a2e58baee42488225e992d15169e39fc0e
Guy Meiri <guymeiri@microsoft.com>
2022-09-12 23:07:34 +0300

    add new todo input

--- src/components/Todo/TodoList.test.tsx
+++ src/components/Todo/TodoList.test.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import TodoList from './TodoList';
 import { render, screen } from '../../../__tests__/utils/testRenderer';
+// import userEvent from '@testing-library/user-event';
 
 describe('TodoList', () => {
   it('should have add todo button', () => {
     render(<TodoList />);
     expect(screen.getByTestId('add-todo-button')).toBeInTheDocument();
   });
+
+  it('should have input field to insert a new todo button', () => {
+    render(<TodoList />);
+    expect(screen.getByTestId('add-todo-input')).toBeInTheDocument();
+  });
+  //   it('should create new todo item', ()=>{
+
+  //   })
 });
--- src/components/Todo/TodoList.tsx
+++ src/components/Todo/TodoList.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getTodos } from '../../redux/reducers/todoSlice';
 
 export default function TodoList() {
-  // const todos = useSelector(getTodos);
+  const todos = useSelector(getTodos);
+  console.log(todos);
   return (
     <div data-hook="todo-page">
-      <button data-hook="add-todo-button"></button>
+      <input data-hook="add-todo-input" type="text"></input>
+      <button data-hook="add-todo-button">Add Todo</button>
     </div>
   );
 }
