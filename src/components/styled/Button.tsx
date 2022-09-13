b48d6673f1cc8a71b89ae0d5a2475bcaefef7e6a
Guy Meiri <guymeiri@microsoft.com>
2022-09-14 01:04:24 +0300

    revert my button test changes

--- src/components/styled/Button.tsx
+++ src/components/styled/Button.tsx
@@ -10,12 +10,12 @@ export const Button = styled.button.attrs<Props, Props>(props => ({
 }))`
   /* This renders the buttons above... Edit me! */
   display: inline-block;
-  border-radius: 4px;
+  border-radius: 3px;
   padding: 0.5rem 0;
   margin: 0.5rem 1rem;
   width: 11rem;
   background: transparent;
-  color: red;
+  color: green;
   border: 2px solid white;
 
   /* The GitHub button is a primary button
